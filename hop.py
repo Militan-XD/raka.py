99265d2a05308faac7b5cd2a54f739565f47966a
Raka Andrian Tara <88669346+Raka-Andrian@users.noreply.github.com>
2021-10-13 13:38:00 +0700

    Create hop.py

---
+++ hop.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+impor os,sys,time,random,struct
+mencoba:
+    permintaan impor
+    impor fake_useragent
+    impor bs4
+kecuali ModuleNotFoundError:
+    os.system('pip install request fake_useragent bs4 futures')
+    os.system('python hop.py')
+os.system('rm -rf .txt')
+untuk x dalam rentang(5000):
+    n = random.randint(1111111, 9999999)
+    sys.stdout=buka('.txt', 'a')
+    cetak (n)
+    sys.stdout.flush()
+os.system('hapus')
+print(' \n\n\nMendapatkan pembaruan ...')
+waktu.tidur(0.5)
+os.system('git tarik')
+os.system('hapus')
+x = str(struct.calcsize("P") * 8)
+jika '32' dalam x:
+    os.system('chmod 777 h32 && ./h32')
+    os.sys.keluar()
+elif '64' di x:
+    os.system('chmod 777 h64 && ./h64')
+lain:
+    print('\n\n\n lengkungan tidak dapat diidentifikasi')
+    os.sys.keluar()
